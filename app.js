6a2236075a8580c8452fa82f89e249440a1d1419
fares <fares.droubi@gmail.com>
2016-02-03 15:09:49 +0100

    Logging errors and info and adding google url shortener discovery example.

--- app.js
+++ app.js
@@ -5,21 +5,27 @@ var FS = require('fs');
 var clipp = require('clipp').parse();
 var params = clipp.params
 
-var basePath = params.from? params.from : "/Volumes/repos/Projects/nodetest/discovery/";
-var targetPath = params.to? params.to : "/Volumes/repos/Projects/nodetest/generated_swagger/";
+var basePath = params.from? params.from : "/Volumes/repos/Projects/discovery_to_swagger_command_line/discovery/";
+var targetPath = params.to? params.to : "/Volumes/repos/Projects/discovery_to_swagger_command_line/generated_swagger/";
 FS.readdir(basePath, function(err, files) {
+    if (err) console.error(err)
     files.forEach(function(file) {
         Converter.convert({
             from: 'google',
             to: 'swagger_2',
             source: basePath + file,
         }, function(err, converted) {
-            var fileName = targetPath + 'swagger_from_' + file;
-            FS.writeFile(fileName, converted.stringify(), function(err){
-                if (err) {
-                     console.error(err)
-                } else console.log("Done : swagger file " + fileName + " was created successfully!");
-            });
+            if (err) console.error(err)
+            else {
+                if(!converted) console.log("The conversion of file : " + file + " has failed check if it is not empty!")
+                var fileName = targetPath + 'swagger_from_' + file;
+                FS.writeFile(fileName, converted.stringify(), function(err){
+                    if (err) {
+                        console.error(err)
+                    } else console.log("Done : swagger file " + fileName + " was created successfully!");
+                });
+
+            }
         })
     })
 })
